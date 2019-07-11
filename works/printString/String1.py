48d351fbd00c7bd914f576e9996cc2c1f76a26fa
Warbler14 <firefly11a@naver.com>
2019-07-11 20:47:53 +0900

    start

---
+++ works/printString/String1.py
@@ -0,0 +1,20 @@
+# set number to String
+strData1 = str(12345)
+
+print(strData1)
+
+numberData = 1
+numberData += 10
+print(numberData)
+
+numberData = 10 << 1
+print(numberData)
+
+numberData = 10 << 2
+print(numberData)
+
+numberData = 10 >> 1
+print(numberData)
+
+numberData = 10 >> 2
+print(numberData)
