e249e873fc5587c7c157ef356396619d173dc2b9
Warbler14 <firefly11a@naver.com>
2019-07-11 21:59:15 +0900

    study

--- works/printString/String1.py
+++ works/printString/String1.py
@@ -18,3 +18,14 @@ print(numberData)
 
 numberData = 10 >> 2
 print(numberData)
+
+list1 = ['one', 'two', 'three']
+print(list1)
+print(list1[0] + "," + list1[1] + "," + list1[2])
+
+dictionaries1 = {"one": 1, "two": 2, "three": 3}
+print(dictionaries1)
+print(str(dictionaries1.get("one")) + "," + str(dictionaries1.get("two")) + "," + str(dictionaries1.get("three")))
+
+tuple1 = ('one', "two", 'three')
+print(tuple1)
